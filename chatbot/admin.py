2277224d59b3e9fe42007ccc0866ea5dc29dfd1f
Shadow21mr <shadow21klx@gmail.com>
2024-10-22 10:00:05 +0200

    admin added

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import FAQ, ClientQuestion
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at', 'updated_at')
+    search_fields = ('question', 'answer')
+
+@admin.register(ClientQuestion)
+class ClientQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'client', 'admin_assigned', 'created_at', 'answered_at')
+    search_fields = ('question',)
+    list_filter = ('answered_at', 'admin_assigned')
+
